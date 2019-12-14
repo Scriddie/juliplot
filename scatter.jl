5a2af2470740e3a8fa44efd150e0da60ee8bdc78
Alexander Reisach <alexander.reisach@gmail.com>
2019-12-14 11:40:22 +0100

    add some type checks

--- scatter.jl
+++ scatter.jl
@@ -1,9 +1,13 @@
 using ImageView
-using Images
-using TestImage
 
 x = collect(1:10)
 y = rand(1:10, 10)
+
+mutable struct JuliPlot
+    img
+    size::Tuple{Int64, Int64}
+    range::Tuple{Int64, Int64}
+end
 
 function get_range(x)
     return maximum(x) - minimum(x)
@@ -19,36 +23,42 @@ function scatter(x, y, size_x, size_y)
     for i in 1:x_length
         current_x = x[i]
         current_y = y[i]
-        x_ind = Int(ceil(((current_y-minimum(y))/y_range) * (size_y-1))) + 1
-        y_ind = Int(ceil(((current_x-minimum(x))/x_range) * (size_x-1))) + 1
-        println(x_ind, " ", y_ind)
-        img[x_ind, y_ind] = 1
+        x_ind = Int(ceil(((current_x-minimum(x))/x_range) * (size_x-1))) + 1
+        y_ind = Int(ceil(((current_y-minimum(y))/y_range) * (size_y-1))) + 1
+        img[y_ind, x_ind] = 1
     end
-    return img
+    return JuliPlot(img, (size_x, size_y), (x_range, y_range))
 end
 
-function x_axis(img)
-    rows = size(img)[1]
-    cols = size(img)[2]
+function smoothen(img)
+    # TODO: apply convolutional filters!
+end
+
+function x_axis(plt::JuliPlot)
+    rows = size(plt.img)[1]
+    cols = size(plt.img)[2]
     axis = zeros(1, cols)
     axis[1, :] .= 0.5
-    img = [img; axis]
-    return img
+    plt.img = [plt.img; axis]
+    return plt
 end
 
-function y_axis(img)
-    rows = size(img)[1]
-    cols = size(img)[2]
+function y_axis(plt::JuliPlot)
+    rows = size(plt.img)[1]
+    cols = size(plt.img)[2]
     axis = zeros(rows, 1)
     axis[:, 1] .= 0.5
-    img = [axis img]
-    return img
+    plt.img = [axis plt.img]
+    return plt
 end
 
 function labels(img)
+    x_range = img.range[1]
+    y_range = img.range[2]
+    # TODO: display image labels as numbers :O
 end
 
-img = scatter(x, y, 100, 100)
-img = x_axis(img)
-img = y_axis(img)
-ImageView.imshow(img)
+plt = scatter(x, y, 100, 100)
+plt = x_axis(plt)
+plt = y_axis(plt)
+ImageView.imshow(plt.img)
